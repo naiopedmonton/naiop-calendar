408905bd452ec41d232196980d32db4cbbcdcaa5
Scott Dohei <dohei@dohei>
2024-06-12 16:29:32 -0600

    generate event csv export

--- src/naiop-calendar.php
+++ src/naiop-calendar.php
@@ -32,6 +32,21 @@ function naiop_event_report_column($event) {
 	return "<a href='$report_url'><div class='dashicons dashicons-download'></div>Generate!</a>";
 }
 
+function get_orders_ids_by_product_id( $product_id, $order_status = array( 'draft', 'wc-completed', 'wc-processing' ) ) {
+    global $wpdb;
+    return $wpdb->get_col("
+        SELECT order_items.order_id
+        FROM {$wpdb->prefix}woocommerce_order_items as order_items
+        LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as order_item_meta ON order_items.order_item_id = order_item_meta.order_item_id
+        LEFT JOIN {$wpdb->posts} AS posts ON order_items.order_id = posts.ID
+        WHERE posts.post_type IN ('shop_order', 'shop_order_placehold')
+        AND posts.post_status IN ( '" . implode( "','", $order_status ) . "' )
+        AND order_items.order_item_type = 'line_item'
+        AND order_item_meta.meta_key = '_product_id'
+        AND order_item_meta.meta_value = '$product_id'
+    ");
+}
+
 function handle_report_request() {
 	if (isset($_GET['naiop_event_report'])) {
 		header("Pragma: public");
@@ -42,11 +57,81 @@ function handle_report_request() {
 		header("Content-Disposition: attachment; filename=\"report.csv\";");
 		header("Content-Transfer-Encoding: binary");
 
-		echo "hello " . $_GET['naiop_event_report'];
+		$event_id = $_GET['naiop_event_report'];
+		$post_id = mc_get_event_post( $event_id );
+		$registration = get_post_meta( $post_id, '_mt_registration_options', true );
+		$product_ids = array();
+		foreach( $registration['prices'] as $price_type => $price_config ) {
+			if ( isset($price_config['product_id']) ) {
+				array_push($product_ids, $price_config['product_id']);
+			}
+		}
+
+		$val = "Order ID,First Name,Last Name,Email,Dietary Restrictions\n";
+		$registered_count = 0;
+		$processed_orders = array();
+		foreach( $product_ids as $pid ) {
+			$orders = get_orders_ids_by_product_id($pid);
+			if ( count($orders) > 0 ) {
+				foreach( $orders as $order_id ) {
+					if ( in_array($order_id, $processed_orders) ) {
+						continue;
+					}
+					$order = wc_get_order( $order_id );
+
+					$fname_data = array();
+					$fname_meta = $order->get_meta( 'naiop_event_fname', false );
+					foreach ($fname_meta as $metadata) {
+						if ( is_array($metadata->get_data()['value']) ) {
+							$fname_data = $metadata->get_data()['value'];
+						}
+					}
+					$lname_data = array();
+					$lname_meta = $order->get_meta( 'naiop_event_lname', false );
+					foreach ($lname_meta as $metadata) {
+						if ( is_array($metadata->get_data()['value']) ) {
+							$lname_data = $metadata->get_data()['value'];
+						}
+					}
+					$email_data = array();
+					$email_meta = $order->get_meta( 'naiop_event_email', false );
+					foreach ($email_meta as $metadata) {
+						if ( is_array($metadata->get_data()['value']) ) {
+							$email_data = $metadata->get_data()['value'];
+						}
+					}
+					$diet_data = array();
+					$diet_meta = $order->get_meta( 'naiop_event_diet', false );
+					foreach ($diet_meta as $metadata) {
+						if ( is_array($metadata->get_data()['value']) ) {
+							$diet_data = $metadata->get_data()['value'];
+						}
+					}
+
+					$count = count($fname_data);
+					$registered_count += $count;
+					for ($x = 0; $x < $count; $x++) {
+						$val .= $order_id . ',' . $fname_data[$x] . ',' . $lname_data[$x] . ',' . $email_data[$x] . ',' . $diet_data[$x] . "\n";
+					}
+					
+					array_push($processed_orders, $order_id);
+				}
+			}
+		}
+		if ($registered_count === 0) {
+			$val = "NO TICKETS SOLD\n";
+		}
+		echo $val;
+	}
+}
+
+add_action( 'init', 'naiop_register_actions' );
+function naiop_register_actions() {
+	if (isset($_GET['naiop_event_report'])) {
+		handle_report_request();
 		exit;
 	}
 }
-handle_report_request();
 
 add_shortcode( 'naiop_upcoming', 'naiop_upcoming_events' );
 /**
